2bcac16387559de7c70c90ae6e42ad0e43cb95ce
Karan5663 <132004682+Karan5663@users.noreply.github.com>
2023-08-14 00:16:12 -0700

    Add files via upload

---
+++ lambda_function.py
@@ -0,0 +1,9 @@
+def cube(a):
+    return a*a*a
+print(cube(3))
+
+c=lambda a:a*a*a
+print(c(3))
+print("*******************multiple lambda fuction*************")
+x = lambda a, b : a * b
+print(x(5, 6)) 
---
+++ python_function.py
@@ -0,0 +1,10 @@
+def fun():
+    print("hello")
+fun()
+
+def add(num1,num2):
+    num3=num1+num2
+    return num3
+num1,num2=10,20
+ans=add(num1,num2)
+print("addition is ",ans)
